8ab7332ec2566a05b431ba51cc2a4e197e514bb1
pedroxian <44950875+pedroxian@users.noreply.github.com>
2020-12-13 00:35:22 +0100

    Add files via upload

---
+++ mash-fxp.lua
@@ -0,0 +1,16 @@
+boot -.builder ..DAW:NUC    enter.matrix    <script.action=drop>{
+
+    much.enter/7.bin=fex    root.bin    /rate.if    else.factory:user
+}
+total.command   if.band/proper  RAT:nuc leave.server    /for.use-on.space
+room.verb   category?ajax.aspx  build.database  /self.delete    .use-format.b
+
+<totaly.builded.unit/roll.faction-work.jobs>    steve.jobs/enter.item=matrix
+
+    {
+        ground.global control.configuration exponse.target/=matrix.user
+    }
+space.work  banch.marked:unit   cage.var(2) item.class(c.1)
+mode.leap   ambulancies ..RAW:nuc category.kloud    filemanager/.uppend.star
+
+;end
